ebd3722be4fb4115e16f1f48880089bf9813bbc7
Rodion <redia1994@mail.ru>
2021-03-19 14:37:16 +0300

    createSlider

--- js/script.js
+++ js/script.js
@@ -310,6 +310,54 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // fetch('http://localhost:3000/menu').then(data => data.json()).then(res => console.log(res));
 
+    // slider
 
+    const slides = document.querySelectorAll('.offer__slide'),
+          prev = document.querySelector('.offer__slider-prev'),
+          next = document.querySelector('.offer__slider-next'),
+          current = document.querySelector('#current'),
+          total = document.querySelector('#total');
+
+    let slideIndex = 1;
+    if (slides.length < 10) {
+        total.textContent = `0${slides.length}`;
+    } else {
+        total.textContent = slides.length;
+    }
+
+
+    slider(slideIndex);
+
+    function slider(n) {
+        if (n> slides.length) {
+            slideIndex = 1;
+        }
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+        slides.forEach(item => item.style.display = 'none');
+        slides[slideIndex -1].style.display = 'block';
+
+        if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`;
+        } else {
+            current.textContent = slideIndex;
+        }
+    
+
+    }
+
+    function nextSlide(n) {
+        slider(slideIndex += n);
+    }
+
+    prev.addEventListener('click', () => {
+        nextSlide(-1);
+    });
+    
+    next.addEventListener('click', () => {
+        nextSlide(1);
+    });
 
 });
+
